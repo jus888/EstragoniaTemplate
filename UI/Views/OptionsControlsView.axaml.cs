5d6777bc475adf4aaf0a771586ad5cf499d9c0b0
jus888 <4376095+jus888@users.noreply.github.com>
2025-03-30 20:53:25 +0200

    ScrollViewer puts space around focussed control

--- UI/Views/OptionsControlsView.axaml.cs
+++ UI/Views/OptionsControlsView.axaml.cs
@@ -1,8 +1,26 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
 using CommunityToolkit.Mvvm.ComponentModel;
+using System.Diagnostics;
 
 namespace EstragoniaTemplate.UI.Views;
 
 public partial class OptionsControlsView : View
 {
+    private const int focusInflation = 30;
+
+    public OptionsControlsView()
+    {
+        InitializeComponent();
+
+        scrollViewer.BringIntoViewOnFocusChange = false;
+        scrollViewer.GotFocus += (s, e) =>
+        {
+            var control = e.Source as Control;
+            var inflatedSize = control?.DesiredSize.Inflate(new(focusInflation)) ?? default;
+            control?.BringIntoView(new Rect(-new Point(focusInflation, focusInflation), inflatedSize));
+        };
+    }
 }
