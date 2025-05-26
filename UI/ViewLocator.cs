4287317ffa6d9c393ab4ca1b8715543075c6ce10
jus888 <4376095+jus888@users.noreply.github.com>
2025-05-26 11:00:22 +0200

    ViewLocator caches views for unique ViewModel instances
    Makes it possible to retain the last focused elements of a view, even when it is temporarily removed from the visual tree due to navigation (can be returned to by closing other ViewModels).

--- UI/ViewLocator.cs
+++ UI/ViewLocator.cs
@@ -2,17 +2,22 @@ using Avalonia.Controls;
 using Avalonia.Controls.Templates;
 using EstragoniaTemplate.UI.ViewModels;
 using System;
+using System.Collections.Generic;
 
 namespace EstragoniaTemplate.UI;
 
+/// <summary>
+/// Caches views for unique ViewModel instances. <br/>
+/// That means that returning to an existing ViewModel by closing a ViewModel <br/>
+/// in a NavigatorViewModel will keep using the same view (allowing for retained focus).
+/// </summary>
 public class ViewLocator : IDataTemplate
 {
+    private readonly Dictionary<Type, object> _viewModelTypeToInstance = new();
+    private readonly Dictionary<Type, Control> _viewModelTypeToView = new();
 
-    public Control? Build(object? param)
+    private Control CreateView(object? param)
     {
-        if (param is null)
-            return null;
-
         var name = param.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
         var type = Type.GetType(name);
 
@@ -22,6 +27,24 @@ public class ViewLocator : IDataTemplate
         }
 
         return new TextBlock { Text = "Not Found: " + name };
+    }
+
+    public Control? Build(object? param)
+    {
+        if (param is null)
+            return null;
+
+        var type = param.GetType();
+        if (!_viewModelTypeToInstance.TryAdd(type, param) && _viewModelTypeToInstance[type] == param)
+        {
+            return _viewModelTypeToView[type];
+        }
+
+        var view = CreateView(param);
+        _viewModelTypeToView[type] = view;
+        _viewModelTypeToInstance[type] = param;
+
+        return view;
     }
 
     public bool Match(object? data)
